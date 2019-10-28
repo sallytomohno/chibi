2905ad6af3c7daaf5e2915ecffa51b5fbc8e083c
TAMURA Sarina <21716060@jwu.ac.jp>
2019-10-28 16:13:09 +0900

    paser123

--- paser.py
+++ paser.py
@@ -3,6 +3,7 @@ from exp import Val, Add
 def paser(s: str):
     num=int(s)
     return Val(num)
+
 e=paser("123")
 print(e)
 
@@ -14,9 +15,15 @@ s1=s[0:pos]
 s2=s[pos+1:]
 print(s,s1,s2)
 
-parse("1")
-parse("1+2")
-parse("1+2+3")
+def parse(s:str):
+    pos=s.find('+')
+    if pos==-1:
+        num=int(s)
+        return Val(num)
+    else:
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        return Add(Val(int(s1)),Val(int(s2)))
 
-
-
+e=parse("123+456+789")
+print(e)
