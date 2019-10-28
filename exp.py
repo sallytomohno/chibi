26a216d69c7ef37b08bae8744a67a2ff8e8e9150
TAMURA Sarina <21716060@jwu.ac.jp>
2019-10-28 15:45:19 +0900

    exp

---
+++ exp.py
@@ -0,0 +1,63 @@
+class Expr :
+    def eval(self) :
+        pass
+    
+def expr(x):
+    if not isinstance(x, Expr) :
+        x = Val(x)
+    return x
+class Binary(Expr) :
+    def __init__(self, left, right) :
+        self.left = expr(left)
+        self.right = expr(right)
+    def __repr__(self) :
+        classname = self.__class__.__name__
+        return f'{classname}({self.left} , {self.right})'
+class Val(Expr) :
+    __slots__ = ["value"]
+    def __init__(self, value = 0) :
+        self.value = value
+    def eval(self) :
+        return self.value
+    def __repr__(self) :
+        return f'Val({self.value})'
+class Add(Binary) :
+    __slots__ = ["left", "right"]
+    def __init__(self, left, right) :
+        self.left = expr(left)
+        self.right = expr(right)
+    def eval(self) :
+        return self.left.eval() + self.right.eval()
+class Mul(Binary) :
+    __slots__ = ["left", "right"]
+    def __init__(self, left, right) :
+        self.left = expr(left)
+        self.right = expr(right)
+    
+    def eval(self) :
+        return self.left.eval() * self.right.eval()
+class Sub(Binary) :
+    __slots__ = ["left", "right"]
+    def __init__(self, left, right) :
+        self.left = expr(left)
+        self.right = expr(right)
+    
+    def eval(self) :
+        return self.left.eval() - self.right.eval()
+    
+class Div(Binary) :
+    __slots__ = ["left", "right"]
+    def __init__(self, left, right) :
+        self.left = expr(left)
+        self.right = expr(right)
+        
+    def eval(self) :
+        return self.left.eval() // self.right.eval()
+e = Add(1, 2)
+assert e.eval () == 3
+print("OK")
+折りたたむ
+
+
+
+
