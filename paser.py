7e7675370720503a7b8c283ce5718b265d891b99
TAMURA Sarina <21716060@jwu.ac.jp>
2019-10-28 16:49:59 +0900

    paser

--- paser.py
+++ paser.py
@@ -1,29 +1,29 @@
 from exp import Val, Add,Sub,Mul,Div
 
-def paser(s: str):
-   if s.find('+')>0:
+def parse(s: str):
+    if s.find('-') > 0:
+       pos=s.find('-')
+       s1=s[0:pos]
+       s2=s[pos+1:]
+       return Sub(parse(s1),parse(s2))
+    if s.find('+') > 0:
        pos=s.find('+')
        s1=s[0:pos]
        s2=s[pos+1:]
        return Add(parse(s1),parse(s2))
-    if s.find('*')>0:
+    if s.find('*') > 0:
        pos=s.find('*')
        s1=s[0:pos]
        s2=s[pos+1:]
        return Mul(parse(s1),parse(s2))
     return Val(int(s))
-    if s.find('-')>0:
-       pos=s.find('-')
-       s1=s[0:pos]
-       s2=s[pos+1:]
-       return Sub(parse(s1),parse(s2))
-    if s.find('/')>0:
+    if s.find('/') > 0:
        pos=s.find('/')
        s1=s[0:pos]
        s2=s[pos+1:]
        return Div(parse(s1),parse(s2))
 
-e=paser("1-2-3")
+e=parse("1-2-3")
 print(e,e.eval())
-assert e.eval()==-4
+assert e.eval()== -4
 
