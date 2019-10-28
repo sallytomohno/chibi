6c56cc5d31bb73c76f37a1775e74cb89b52e8b9f
TAMURA Sarina <21716060@jwu.ac.jp>
2019-10-28 16:40:31 +0900

    paser

--- paser.py
+++ paser.py
@@ -1,29 +1,29 @@
-from exp import Val, Add
+from exp import Val, Add,Sub,Mul,Div
 
 def paser(s: str):
-    num=int(s)
-    return Val(num)
+   if s.find('+')>0:
+       pos=s.find('+')
+       s1=s[0:pos]
+       s2=s[pos+1:]
+       return Add(parse(s1),parse(s2))
+    if s.find('*')>0:
+       pos=s.find('*')
+       s1=s[0:pos]
+       s2=s[pos+1:]
+       return Mul(parse(s1),parse(s2))
+    return Val(int(s))
+    if s.find('-')>0:
+       pos=s.find('-')
+       s1=s[0:pos]
+       s2=s[pos+1:]
+       return Sub(parse(s1),parse(s2))
+    if s.find('/')>0:
+       pos=s.find('/')
+       s1=s[0:pos]
+       s2=s[pos+1:]
+       return Div(parse(s1),parse(s2))
 
-e=paser("123")
-print(e)
+e=paser("1-2-3")
+print(e,e.eval())
+assert e.eval()==-4
 
-s="1+2"
-pos=s.find('+')
-print('pos',pos)
-
-s1=s[0:pos]
-s2=s[pos+1:]
-print(s,s1,s2)
-
-def parse(s:str):
-    pos=s.find('+')
-    if pos==-1:
-        num=int(s)
-        return Val(int(s))
-    else:
-        s1=s[0:pos]
-        s2=s[pos+1:]
-        return Add(parse(s1),parse(s2))
-
-e=parse("1+2+3")
-print(e, e.val())
