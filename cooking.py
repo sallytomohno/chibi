9cd8b22859e8f9ff8aebe58bdc6e4de83ea81d79
TAMURA Sarina <21716060@jwu.ac.jp>
2020-01-16 11:48:48 +0900

    kare-

--- cooking.py
+++ cooking.py
@@ -1,7 +1,13 @@
+import random
+s=[1,2,3,4,5]
+z=[1,2,3,4,5,6,7,8,9,10]
+n=[1,2,3,4,5]
+p=[1,2,3,4,5]
+t=[1,2,3,4,5,6,7,8,9,10,11]
+d=[1,2,3,4,5,6,7,8,9,10]
+print('sarada=',random.choice(s),'zensai=',random.sample(z,2),'niku=',random.choice(n),'pizza=',random.choice(p),'tansuikabutu=',random.choice(t),'dezart=',random.choice(d))
+
 import requests
-from bs4 import BeautifulSoup
-import sys
-from time import sleep
  
 query = "カレー"
 url = "https://cookpad.com/" #クックパッド
